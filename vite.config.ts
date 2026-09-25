da0d3cfb79542e5c5f44f689182567b180497be3
HarisHerdiansyah <haris54237@gmail.com>
2026-09-25 07:23:07 +0700

    init shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'node:path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   server: {
-    port: 3000
-  }
+    port: 3000,
+  },
 });
